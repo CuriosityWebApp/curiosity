2a9afb31023302512a9754b8613a26c599042401
Justin Chung <bearlyalive1337@gmail.com>
2018-10-02 18:08:57 -0400

    (refactor) Removed index.html from gitignore

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,35 +7,35 @@ import QuestionList from './components/Question/QuestionList.jsx';
 import AnswerList from './components/Answer/AnswerList.jsx';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:3000/graphql'
+  uri: 'http://localhost:3000/graphql',
 });
 
 class App extends Component {
-	constructor() {
-		super();
-	}
-	render() {
-		return (
-			<div>
-		    <div className="jumbotron jumbotron-fluid" id="home-jumbo">
-		    	<div className="container" style={{ marginLeft: 50, marginRight: 25 }}>
-		    		<h1 className="display-4">Curiosity</h1>
-		    		<p className="lead">Hello world</p>
-		    		<div className="pb-1"></div>
-		    		{/* <Signup emailAndPassSignUp={props.emailAndPassSignUp} googleSignUp={props.googleSignUp} /> */}
-		    	</div>
-		    </div>
-			  <div>
-			  	<QuestionList />
-			  </div>
-			</div>
-		);
-	}
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid" id="home-jumbo">
+          <div className="container" style={{ marginLeft: 50, marginRight: 25 }}>
+            <h1 className="display-4">Curiosity</h1>
+            <p className="lead">Hello world</p>
+            <div className="pb-1" />
+            {/* <Signup emailAndPassSignUp={props.emailAndPassSignUp} googleSignUp={props.googleSignUp} /> */}
+          </div>
+        </div>
+        <div>
+          <QuestionList />
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-	<ApolloProvider client={client}>
-		<App />
-	</ApolloProvider>,
-	document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
