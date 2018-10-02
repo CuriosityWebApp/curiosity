b3d119f35fe5bc3c4819cf77b5bc1b94ec54bc15
Jay <hyunjae9034@gmail.com>
2018-10-02 14:57:29 -0400

    basic css

--- client/src/components/Question/QuestionItem.jsx
+++ client/src/components/Question/QuestionItem.jsx
@@ -4,22 +4,23 @@ import moment from 'moment'
 class QuestionItem extends Component {
 	render() {
 		let {postData} = this.props
-		console.log(postData)
 		return (
-			<div className="list-group">
-			  <div className="list-group-item list-group-item-action flex-column align-items-start" onClick={() => this.props.onSelect(postData.id)}>
-			  	<div className="d-flex w-100 justify-content-between">
-			  		<h5>{postData.questionTitle}</h5>
-			  	</div>
-					<div>
-						<small className="text-muted">Posted By {postData.user.username} {moment(postData.createdAt).fromNow()}</small>
-			  		<small className="text-muted"> Rank {postData.restriction} </small>
-			  		<small className="text-muted"> Reward {postData.bounty} </small>
-			  		<small className="text-muted"> Answers {postData.answers.length}</small>
-			  		<p>{postData.questionContent}</p>
-					</div>
+			<a href="#">
+			  <div className="list-group">
+			    <div className="list-group-item list-group-item-action flex-column align-items-start" onClick={() => this.props.onSelect(postData.id)}>
+			    	<div className="d-flex w-100 justify-content-between">
+			    		<h5>{postData.questionTitle}</h5>
+			  			<h7>Reward: {postData.bounty}</h7>
+			    	</div>
+			  		<div>
+			  			<small className="text-muted d-flex w-100 justify-content-between">Posted By {postData.user.username} {moment(postData.createdAt).fromNow()}</small>
+			    		<small className="text-muted"> Rank {postData.restriction} </small>
+			    		<small className="text-muted"> Answers {postData.answers.length}</small>
+			    		{/* <p>{postData.questionContent}</p> */}
+			  		</div>
+			    </div>
 			  </div>
-			</div>
+			</a>
 		);
 	}
 };
