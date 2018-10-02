75549ed13827d832e3a71c0cc137f74b21e62b56
Jay <hyunjae9034@gmail.com>
2018-10-02 16:18:01 -0400

    added more css on question

--- client/src/components/Question/QuestionItem.jsx
+++ client/src/components/Question/QuestionItem.jsx
@@ -10,7 +10,7 @@ class QuestionItem extends Component {
 			    <div className="list-group-item list-group-item-action flex-column align-items-start" onClick={() => this.props.onSelect(postData.id)}>
 			    	<div className="d-flex w-100 justify-content-between">
 			    		<h5>{postData.questionTitle}</h5>
-			  			<h7>Reward: {postData.bounty}</h7>
+			  			<h6>Reward: {postData.bounty}</h6>
 			    	</div>
 			  		<div>
 			  			<small className="text-muted d-flex w-100 justify-content-between">Posted By {postData.user.username} {moment(postData.createdAt).fromNow()}</small>
@@ -26,30 +26,3 @@ class QuestionItem extends Component {
 };
 
 export default QuestionItem;
-
-{/* <div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small>3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small>Donec id elit non mi porta.</small>
-  </a>
-  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small class="text-muted">3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small class="text-muted">Donec id elit non mi porta.</small>
-  </a>
-  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small class="text-muted">3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small class="text-muted">Donec id elit non mi porta.</small>
-  </a>
-</div> */}
