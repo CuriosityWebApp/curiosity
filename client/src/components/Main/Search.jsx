2633226f0b8d27277b83484880b973fcdb7bc5cb
Justin Chung <bearlyalive1337@gmail.com>
2018-10-04 17:51:20 -0400

    (feat) search on the way

--- client/src/components/Main/Search.jsx
+++ client/src/components/Main/Search.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import { searchQuestion } from '../../queries/queries.js';
 import { client, withApollo } from 'react-apollo';
-
+import Autocomplete from 'react-autocomplete';
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       term: '',
+      questions: [],
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.executeSearch = this.executeSearch.bind(this);
@@ -28,7 +29,9 @@ class Search extends React.Component {
       })
       .then(({ data }) => {
         if (data.searchQuestion) {
-          console.log(data.searchQuestion);
+          this.setState({
+            questions: data.searchQuestion,
+          });
         } else {
           console.log('waiting for term to fetch user data');
         }
@@ -47,12 +50,18 @@ class Search extends React.Component {
         <label className="sr-only" htmlFor="inlineFormInputName2">
           Name
         </label>
-        <input
-          className="form-control"
-          type="text"
+        <Autocomplete
+          items={this.state.questions}
+          getItemValue={item => item.questionTitle}
+          renderItem={(item, highlighted) => (
+            <div key={item.id} style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}>
+              {item.questionTitle}
+            </div>
+          )}
+          wrapperStyle={{ position: 'relative', display: 'inline-block' }}
           value={this.state.term}
           onChange={this.handleInputChange}
-          placeholder="Search Questions"
+          inputProps={{ className: 'form-control', placeholder: 'askQuestion' }}
         />
         <button type="submit" className="btn btn-primary mb-2" onClick={this.executeSearch}>
           Submit
